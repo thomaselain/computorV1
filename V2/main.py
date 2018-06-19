e21b68f1df029802a3c5d01fde02945f2b8a13e1
Thomas <elainthomas0@gmail.com>
2018-06-19 16:44:30 +0200

    apres la cafet

--- V2/main.py
+++ V2/main.py
@@ -7,13 +7,13 @@ import re
 arg = "".join(sys.argv[1].split()) 
 
 regex = re.compile('([+-]*[0-9\.]+\/*[0-9\.]*)(?:\*?([Xx](?:\^([0-9]+))?))?')
-is_valid = re.compile('^([+-]*(?:[0-9]+\*?[xX]\^[0-2]|[0-9]+\*?[xX]|[xX]|[0-9]+))+=([+-]*(?:[0-9]+\*?[xX]\^[0-2]|[0-9]+\*?[xX]|[xX]|[0-9]+))+$')
+is_valid = re.compile('^([+-]*(?:[0-9]+\*?[xX]\^[0-9]+|[0-9]+\*?[xX]|[xX]|[0-9]+))+=([+-]*(?:[0-9]+\*?[xX]\^[0-9]+|[0-9]+\*?[xX]|[xX]|[0-9]+))+$')
 
 errors_search()
 
-# if len(is_valid.findall(arg)) == 0:
-# 	print("ntm c pa bon")
-# 	exit()
+if len(is_valid.findall(arg)) == 0:
+	print("ntm c pa bon")
+	exit()
 
 left = regex.findall(arg[:arg.find('=')])
 right = regex.findall(arg[arg.find('='):])
--- V2/process.py
+++ V2/process.py
@@ -16,8 +16,11 @@ def process(Terms):
 	for term in Terms:
 		if term.side == RIGHT:
 			term.change_side()
-		reduced[int(term.expo)].num += float(term.num)
-
+		if (int(term.expo) <= 2):
+			reduced[int(term.expo)].num += float(term.num)
+		else:
+			print("This program can only handle equations of 2nd degree or lower")
+			exit()
 	# for term in reduced:
 	# 	term.debug()
 
@@ -30,7 +33,8 @@ def process(Terms):
 		res += ("" if reduced[2].num == 0 else "+ " if reduced[1].num >= 0 else "- ") + str(abs(reduced[1].num)) + "x "
 	if reduced[0].exists():
 		res += ("" if reduced[2].num == 0 and reduced[1].num == 0 else "+ " if reduced[0].num >= 0 else "- ") + str(abs(reduced[0].num)) + " "
-
+	if not reduced[2].exists() and not reduced[1].exists() and not reduced[0].exists():
+		res += "0 "
 	sys.stdout.write("Reduced form : ")
 	print(res + "= 0")
 	# End of printing
