0bb1691c152428960bc1b48d13fd92659cbfd1f1
Thomas <elainthomas0@gmail.com>
2018-06-25 14:39:27 +0200

    truc

--- V2/main.py
+++ V2/main.py
@@ -13,15 +13,15 @@ arg = "".join(sys.argv[1].split())
 
 # Ca, mec, c'est beau (tres beau) (et satrouv un coef avec d truc de ouf genre des / ou des - ou des . ou tout a la fois (c fou))
 # [0-9]+(?:[\.][+-]?[0-9]+)?(?:\/[-+]?[0-9]+(?:[\.][0-9]+)?)?
-
-regex = re.compile('([+-]*)([0-9]*)(\*?[xX]\^[0-2]|[xX])?')
-is_valid = re.compile('^([+-]*(?:[0-9]+(?:[\.][0-9]+)?(?:\/[0-9]+(?:[\.][0-9]+)?)?\*?[xX]\^[0-9]+|[0-9]+\*?[xX]|[xX]|[0-9]+))+=([+-]*(?:[0-9]+\*?[xX]\^[0-9]+|[0-9]+\*?[xX]|[xX]|[0-9]+))+$')
+one_term_regex = "([+-]*)([0-9]*)(\*?[xX]\^[0-2]|[xX])?"
+regex = re.compile(one_term_regex)
+is_valid = re.compile('^(' + one_term_regex + ")+=(" + one_term_regex + ")+$")
 
 errors_search()
 
-# if len(is_valid.findall(arg)) == 0:
-# 	print("ntm c pa bon")
-# 	exit()
+if len(is_valid.findall(arg)) == 0:
+	print("ntm c pa bon")
+	exit()
 
 left = regex.findall(arg[:arg.find('=')])
 right = regex.findall(arg[arg.find('='):])
